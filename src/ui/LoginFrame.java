16e68d7ec79293a37575c9664b48101306eb46f7
Carlos Lucena Robles <carlos.lucena022@gmail.com>
2025-05-14 16:46:22 +0200

    Added login frame

---
+++ src/ui/LoginFrame.java
@@ -0,0 +1,138 @@
+package ui;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import components.*;
+
+public class LoginFrame extends BaseFrame {
+
+    private final Box contentBox;
+
+    public LoginFrame(String title) {
+        super(title);
+    
+        contentBox = Box.createVerticalBox();
+    
+        addTitle();
+        addSpacing(16);
+
+        addSubtitle();
+        addSpacing(33);
+
+        addEmailField();
+        addSpacing(25);
+
+        addPasswordField();
+        addSpacing(9);
+
+        errorMessage("El correo y/o la contraseña son incorrectos");
+        addSpacing(25);
+
+        addLoginButton();
+        addSpacing(31);
+
+        addRegisterLink();
+
+        // Centrar Box en medio de la ventana con un JPanel
+        JPanel centerPanel = new JPanel();
+
+        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+        centerPanel.setBackground(Color.WHITE);
+
+        centerPanel.add(Box.createVerticalGlue());
+        centerPanel.add(contentBox);
+        centerPanel.add(Box.createVerticalGlue());
+
+        contentBox.setAlignmentX(Component.CENTER_ALIGNMENT);
+        contentBox.setMaximumSize(new Dimension(342, 445));
+
+        add(centerPanel, BorderLayout.CENTER);
+    }
+    
+
+    private void addTitle() {
+        JLabel titleLabel = new JLabel("Bienvenido de nuevo 🐾");
+        titleLabel.setFont(new Font("Sora SemiBold", Font.PLAIN, 24));
+        titleLabel.setForeground(Color.decode("#0C1421"));
+        contentBox.add(titleLabel);
+    }
+
+    private void addSubtitle() {
+        JLabel subtitle = new JLabel("<html><div>Introduce tus credenciales para iniciar sesión o regístrate si aún no tienes cuenta</div></html>");
+        subtitle.setFont(new Font("Poppins Regular", Font.PLAIN, 15));
+        subtitle.setForeground(Color.decode("#313957"));
+        subtitle.setPreferredSize(new Dimension(342, 48));
+        subtitle.setAlignmentX(Component.LEFT_ALIGNMENT);
+        contentBox.add(subtitle);
+    }
+
+    private void addEmailField() {
+        JLabel emailLabel = new JLabel("Correo");
+        emailLabel.setFont(new Font("Roboto Regular", Font.PLAIN, 14));
+        emailLabel.setForeground(Color.decode("#0C1421"));
+        emailLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        RoundedTextField emailField = new RoundedTextField(18);
+        emailField.setForeground(Color.GRAY);
+        emailField.setPlaceholder("correo@email.com");
+        emailField.setBackground(Color.decode("#F3F7FB"));
+        emailField.setPreferredSize(new Dimension(342, 42));
+        emailField.setBorderColor(Color.decode("#D4D7E3"));
+        emailField.setBorderWidth(2.0f);
+
+        contentBox.add(emailLabel);
+        contentBox.add(emailField);
+    }
+
+    private void addPasswordField() {
+        JLabel passLabel = new JLabel("Contraseña");
+        passLabel.setFont(new Font("Roboto Regular", Font.PLAIN, 14));
+        passLabel.setForeground(Color.decode("#0C1421"));
+        passLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        RoundedPasswordField passwordField = new RoundedPasswordField(18);
+        passwordField.setEchoChar((char) 0);
+        passwordField.setPlaceholder("Contraseña");
+        passwordField.setForeground(Color.GRAY);
+        passwordField.setBackground(Color.decode("#F3F7FB"));
+        passwordField.setPreferredSize(new Dimension(342, 42));
+        passwordField.setBorderColor(Color.decode("#D4D7E3"));
+        passwordField.setBorderWidth(2.0f);
+
+        contentBox.add(passLabel);
+        contentBox.add(passwordField);
+    }
+
+    private void addLoginButton() {
+        RoundedButton loginButton = new RoundedButton("Iniciar sesión", 20);
+        loginButton.setBackground(Color.decode("#313131"));
+        loginButton.setForeground(Color.WHITE);
+        loginButton.setFont(new Font("Roboto Regular", Font.PLAIN, 14));
+        loginButton.setPreferredSize(new Dimension(350, 44));
+        loginButton.setMaximumSize(new Dimension(342, 44));
+
+        contentBox.add(loginButton);
+    }
+
+    private void errorMessage(String message) {
+        JLabel errorLabel = new JLabel(message);
+        errorLabel.setFont(new Font("Roboto Regular", Font.PLAIN, 13));
+        errorLabel.setForeground(Color.RED);
+        errorLabel.setPreferredSize(new Dimension(254, 21));
+        errorLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        contentBox.add(errorLabel);
+    }
+
+    private void addRegisterLink() {
+        JLabel registerLabel = new JLabel("<html>¿No tienes ninguna cuenta? <a href='#'>Registrarse</a></html>");
+        registerLabel.setFont(new Font("Roboto Regular", Font.PLAIN, 16));
+        registerLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        contentBox.add(registerLabel);
+    }
+
+    private void addSpacing(int height) {
+        contentBox.add(Box.createRigidArea(new Dimension(0, height)));
+    }
+}
