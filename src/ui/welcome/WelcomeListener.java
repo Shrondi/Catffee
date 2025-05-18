a2c163e1327ba5276d7b53f9bf14708699f380f7
Carlos Lucena Robles <carlos.lucena022@gmail.com>
2025-05-18 14:28:48 +0200

    Fixed code

--- src/ui/welcome/WelcomeListener.java
+++ src/ui/welcome/WelcomeListener.java
@@ -4,17 +4,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import ui.login.LoginFrame;
-import ui.login.LoginListener;
-import utils.UserStorage;
 
 class WelcomeListener implements ActionListener {
 
     private final WelcomeFrame welcomeFrame;
-    private final UserStorage userStorage;
 
     WelcomeListener(WelcomeFrame welcomeFrame) {
         this.welcomeFrame = welcomeFrame;
-        this.userStorage = UserStorage.getInstance();
     }
 
     // Este método se llama automáticamente cuando se pulsa el botón
@@ -22,10 +18,6 @@ class WelcomeListener implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         LoginFrame login = new LoginFrame("Iniciar sesión");
         login.setVisible(true);
-
-        login.getLoginButton().addActionListener(
-            new LoginListener(login)
-        );
 
         welcomeFrame.setVisible(false);
         welcomeFrame.dispose(); // Liberar recursos
