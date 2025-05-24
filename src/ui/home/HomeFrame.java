aeed01d8334b35a4c8703fa837fffb260c84f4db
Carlos Lucena Robles <carlos.lucena022@gmail.com>
2025-05-24 16:51:42 +0200

    Enhanced and refactored homeFrame

--- src/ui/home/HomeFrame.java
+++ src/ui/home/HomeFrame.java
@@ -1,13 +1,14 @@
 package ui.home;
 
 import javax.swing.*;
+import java.awt.*;
 
-import components.panel.RoundedPanel;
-import ui.BaseFrame;
 import components.bar.NavigationBar;
 import components.button.RoundedButton;
-
-import java.awt.*;
+import components.panel.RoundedPanel;
+import controller.NavigationManager;
+import ui.BaseFrame;
+import utils.UserStorage;
 
 public class HomeFrame extends BaseFrame {
 
@@ -20,22 +21,23 @@ public class HomeFrame extends BaseFrame {
 
         mainPanel.add(topSection());
         mainPanel.add(productScroll());
-        mainPanel.add(new NavigationBar("Inicio"));
 
         add(mainPanel);
+
+        NavigationBar navBar = new NavigationBar("Inicio");
+        new NavigationManager(this, navBar);
+        add(navBar, BorderLayout.SOUTH);
     }
 
     private JPanel topSection() {
         JPanel container = new JPanel();
         container.setLayout(new OverlayLayout(container));
-        container.setPreferredSize(new Dimension(412, 330));
-        container.setMaximumSize(new Dimension(412, 330));
+        Dimension size = new Dimension(412, 330);
+        container.setPreferredSize(size);
+        container.setMaximumSize(size);
 
-        JLabel imageLabel = headerImage();
-        JPanel textPanel = headerText();
-
-        container.add(textPanel);
-        container.add(imageLabel);
+        container.add(headerText());
+        container.add(headerImage());
 
         return container;
     }
@@ -43,27 +45,29 @@ public class HomeFrame extends BaseFrame {
     private JLabel headerImage() {
         ImageIcon icon = new ImageIcon("resources/images/cabecera_bienvenida.png");
         Image scaled = icon.getImage().getScaledInstance(412, 330, Image.SCALE_SMOOTH);
-        JLabel imageLabel = new JLabel(new ImageIcon(scaled));
-        imageLabel.setAlignmentX(0.5f);
-        imageLabel.setAlignmentY(0.5f);
-        return imageLabel;
+        JLabel label = new JLabel(new ImageIcon(scaled));
+        label.setAlignmentX(0.5f);
+        label.setAlignmentY(0.5f);
+        return label;
     }
 
     private JPanel headerText() {
-        JPanel textPanel = new JPanel(new BorderLayout());
-        textPanel.setOpaque(false);
-        textPanel.setAlignmentX(0.5f);
-        textPanel.setAlignmentY(0.5f);
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setOpaque(false);
+        panel.setAlignmentX(0.5f);
+        panel.setAlignmentY(0.5f);
 
-        JLabel welcomeText = new JLabel("<html><div style='text-align: left;'>Bienvenido,<br><b>Nombre ☕</b></div></html>");
+        String nombre = UserStorage.getInstance().getCurrentUser().getNombreCompleto();
+        JLabel welcomeText = new JLabel(
+            String.format("<html><div style='text-align: left;'>Bienvenido,<br><b>%s ☕</b></div></html>", nombre));
         welcomeText.setForeground(Color.WHITE);
         welcomeText.setFont(new Font("Segoe UI Emoji", Font.BOLD, 32));
         welcomeText.setHorizontalAlignment(SwingConstants.LEFT);
         welcomeText.setVerticalAlignment(SwingConstants.BOTTOM);
         welcomeText.setBorder(BorderFactory.createEmptyBorder(0, 40, 40, 0));
 
-        textPanel.add(welcomeText, BorderLayout.CENTER);
-        return textPanel;
+        panel.add(welcomeText, BorderLayout.CENTER);
+        return panel;
     }
 
     private JScrollPane productScroll() {
@@ -77,10 +81,8 @@ public class HomeFrame extends BaseFrame {
         destacadosLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         destacadosLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 16, 0));
 
-        JPanel productGrid = productGrid();
-
         contentPanel.add(destacadosLabel);
-        contentPanel.add(productGrid);
+        contentPanel.add(productGrid());
 
         JScrollPane scrollPane = new JScrollPane(contentPanel);
         scrollPane.setBorder(null);
@@ -113,45 +115,49 @@ public class HomeFrame extends BaseFrame {
         for (int i = 0; i < names.length; i++) {
             grid.add(productBox(names[i], descriptions[i], prices[i], images[i]));
         }
-
         return grid;
     }
 
     private JPanel productBox(String title, String subtitle, String price, String imagePath) {
         RoundedPanel box = new RoundedPanel(16);
         box.setLayout(new BoxLayout(box, BoxLayout.Y_AXIS));
-        box.setPreferredSize(new Dimension(160, 240));
-        box.setMaximumSize(new Dimension(160, 240));
+        Dimension size = new Dimension(170, 250);
+        box.setPreferredSize(size);
+        box.setMaximumSize(size);
         box.setBackground(Color.WHITE);
+        box.setBorder(BorderFactory.createEmptyBorder(10, 10, 40, 20));
 
         box.add(Box.createVerticalStrut(10));
         box.add(productImage(imagePath));
         box.add(Box.createVerticalStrut(5));
-        box.add(createLabel(title, "Sora SemiBold", 16));
+
+        box.add(createLeftAlignedLabel(title, "Sora SemiBold", 16));
         box.add(Box.createVerticalStrut(5));
-        box.add(createLabel(subtitle, "Sora Regular", 12, Color.GRAY));
+        box.add(createLeftAlignedLabel(subtitle, "Sora Regular", 12, Color.GRAY));
         box.add(Box.createVerticalStrut(5));
         box.add(createPricePanel(price));
 
         return box;
     }
 
-    private JLabel productImage(String path) {
-        ImageIcon icon = new ImageIcon(path);
-        Image scaled = icon.getImage().getScaledInstance(140, 130, Image.SCALE_SMOOTH);
-        JLabel img = new JLabel(new ImageIcon(scaled));
-        img.setAlignmentX(Component.CENTER_ALIGNMENT);
-        return img;
+    private JPanel createLeftAlignedLabel(String text, String fontName, int fontSize) {
+        return createLeftAlignedLabel(text, fontName, fontSize, Color.BLACK);
     }
 
-    private JLabel createLabel(String text, String fontName, int fontSize) {
-        return createLabel(text, fontName, fontSize, Color.BLACK);
-    }
-
-    private JLabel createLabel(String text, String fontName, int fontSize, Color color) {
+    private JPanel createLeftAlignedLabel(String text, String fontName, int fontSize, Color color) {
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        panel.setOpaque(false);
         JLabel label = new JLabel(text);
         label.setFont(new Font(fontName, Font.PLAIN, fontSize));
         label.setForeground(color);
+        panel.add(label);
+        return panel;
+    }
+
+    private JLabel productImage(String path) {
+        ImageIcon icon = new ImageIcon(path);
+        Image scaled = icon.getImage().getScaledInstance(140, 130, Image.SCALE_SMOOTH);
+        JLabel label = new JLabel(new ImageIcon(scaled));
         label.setAlignmentX(Component.CENTER_ALIGNMENT);
         return label;
     }
@@ -159,21 +165,25 @@ public class HomeFrame extends BaseFrame {
     private JPanel createPricePanel(String price) {
         JLabel priceLabel = new JLabel(price);
         priceLabel.setFont(new Font("Poppins", Font.BOLD, 18));
+        priceLabel.setForeground(Color.BLACK);
 
         RoundedButton plusButton = new RoundedButton("+", 8);
-        plusButton.setFont(new Font("Poppins", Font.BOLD, 14));
+        plusButton.setFont(new Font("Poppins", Font.PLAIN, 20));
+        plusButton.setMargin(new Insets(0, 0, 0, 0));
         plusButton.setBackground(new Color(0xC67C4E));
         plusButton.setForeground(Color.WHITE);
-        plusButton.setContentAreaFilled(false);
-        plusButton.setOpaque(false);
+        plusButton.setContentAreaFilled(true);
+        plusButton.setOpaque(true);
         plusButton.setBorderPainted(false);
-        plusButton.setPreferredSize(new Dimension(55, 55));
-        plusButton.setMaximumSize(new Dimension(55, 55));
+        
+        Dimension btnSize = new Dimension(30, 30);
+        plusButton.setPreferredSize(btnSize);
+        plusButton.setMaximumSize(btnSize);
 
-        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER, 25, 0));
+        JPanel panel = new JPanel(new BorderLayout());
         panel.setOpaque(false);
-        panel.add(priceLabel);
-        panel.add(plusButton);
+        panel.add(priceLabel, BorderLayout.WEST);
+        panel.add(plusButton, BorderLayout.EAST);
 
         return panel;
     }
